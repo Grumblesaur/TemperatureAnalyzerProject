38356e889d063636d77dbd2520d267bb4e2d9fbc
catlett.rc <catlett.rc@gmail.com>
2016-03-30 17:15:23 -0400

    Editing SQL instructions

---
+++ NetBeans/tablesensor.sql
@@ -0,0 +1,62 @@
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(441384,(select "Symbol" from APP.Location where "Symbol" = 'OFF'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(532797, (select "Symbol" from APP.Location where "Symbol" = 'BMS'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(532798, (select "Symbol" from APP.Location where "Symbol" = 'BPA'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(532800, (select "Symbol" from APP.Location where "Symbol" = 'HRC'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(532801, (select "Symbol" from APP.Location where "Symbol" = 'NPE'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(533551, (select "Symbol" from APP.Location where "Symbol" = 'BPT'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(533560, (select "Symbol" from APP.Location where "Symbol" = 'EMP'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(555494, (select "Symbol" from APP.Location where "Symbol" = 'MEL'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(555497, (select "Symbol" from APP.Location where "Symbol" = 'GRV'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(555498, (select "Symbol" from APP.Location where "Symbol" = 'MER'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(555501, (select "Symbol" from APP.Location where "Symbol" = 'GEP'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(555502, (select "Symbol" from APP.Location where "Symbol" = 'WFM'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES  
+(555505, (select "Symbol" from APP.Location where "Symbol" = 'BOR'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(555506, (select "Symbol" from APP.Location where "Symbol" = 'LSP'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(625100, (select "Symbol" from APP.Location where "Symbol" = 'MOM'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(625102, (select "Symbol" from APP.Location where "Symbol" = 'DIN'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES  
+(695788, (select "Symbol" from APP.Location where "Symbol" = 'OFF'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(710736, (select "Symbol" from APP.Location where "Symbol" = 'MEL'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(710737, (select "Symbol" from APP.Location where "Symbol" = 'GOB'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(710738, (select "Symbol" from APP.Location where "Symbol" = 'OFF'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES  
+(710739, (select "Symbol" from APP.Location where "Symbol" = 'SBM'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES  
+(733935, (select "Symbol" from APP.Location where "Symbol" = 'WME'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES  
+(733936, (select "Symbol" from APP.Location where "Symbol" = 'GBR'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES  
+(733937, (select "Symbol" from APP.Location where "Symbol" = 'MEM'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(733938, (select "Symbol" from APP.Location where "Symbol" = 'OFF'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES  
+(733939, (select "Symbol" from APP.Location where "Symbol" = 'CKL'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES  
+(984395, (select "Symbol" from APP.Location where "Symbol" = 'HWY'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(1001871, (select "Symbol" from APP.Location where "Symbol" = 'BRR'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES  
+(1001872, (select "Symbol" from APP.Location where "Symbol" = 'EDC'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(1001873, (select "Symbol" from APP.Location where "Symbol" = 'CCR'));
+INSERT INTO APP.SENSOR ("Serial_Number", "Location_Symbol") VALUES 
+(1001874, (select "Symbol" from APP.Location where "Symbol" = 'LPJ'));
