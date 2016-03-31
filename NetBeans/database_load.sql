de7f24634aac3080ff359b4168fb71c56784b053
catlett.rc <catlett.rc@gmail.com>
2016-03-30 20:10:52 -0400

    Finished the instructions and made them up to date

--- NetBeans/database_load.sql
+++ NetBeans/database_load.sql
@@ -1,19 +1,18 @@
 CREATE TABLE APP.LOCATION (
-  Symbol VARCHAR(3) NOT NULL unique primary key,
-  Location VARCHAR(45) NOT NULL),
-    CONSTRAINT unique (Symbol);
+  "Symbol" VARCHAR(3) NOT NULL primary key,
+  "Location" VARCHAR(45) NOT NULL);
 
 CREATE TABLE APP.SENSOR (
-  Serial_Number INT NOT NULL primary key,
-  Location_Symbol VARCHAR(3)
-        NOT NULL REFERENCES APP.LOCATION (Symbol));
+  "Serial_Number" INT NOT NULL primary key,
+  "Location_Symbol" VARCHAR(3)
+        NOT NULL REFERENCES APP.LOCATION ("Symbol"));
 
 
 CREATE TABLE APP.MEASUREMENT (
-  Temperature INT NOT NULL,
-  DMY DATE,
-  Clock TIME,
-  Location_Symbol VARCHAR(3) NOT NULL 
-        REFERENCES APP.Location (Symbol));
+  "Temperature" INT NOT NULL,
+  "Date" DATE,
+  "Time" TIME,
+  "Location_Symbol" VARCHAR(3) NOT NULL 
+        REFERENCES APP.Location ("Symbol"));
   
  
