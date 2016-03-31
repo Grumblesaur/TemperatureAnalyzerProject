2e06963a64394cacb2bd2a4211ff5f56ff4e9b74
catlett.rc <catlett.rc@gmail.com>
2016-03-30 20:50:20 -0400

    Finishing Database load

--- NetBeans/database_load.sql
+++ NetBeans/database_load.sql
@@ -3,10 +3,9 @@ CREATE TABLE APP.LOCATION (
   "Location" VARCHAR(45) NOT NULL);
 
 CREATE TABLE APP.SENSOR (
-  "Serial_Number" INT NOT NULL primary key,
+  "Serial_Number" INTEGER NOT NULL primary key,
   "Location_Symbol" VARCHAR(3)
-        NOT NULL REFERENCES APP.LOCATION ("Symbol"));
-
+        REFERENCES APP.LOCATION ("Symbol"));
 
 CREATE TABLE APP.MEASUREMENT (
   "Temperature" INT NOT NULL,
@@ -14,5 +13,3 @@ CREATE TABLE APP.MEASUREMENT (
   "Time" TIME,
   "Location_Symbol" VARCHAR(3) NOT NULL 
         REFERENCES APP.Location ("Symbol"));
-  
- 
